96ca8ed34841166285303f0fee0c01dc05813e6d
Brancori <102886227+brancori@users.noreply.github.com>
2023-06-06 00:30:56 -0600

    team colaborators

--- proyectos/PROYECCION_INTELIGENTE/css/DarkMode.js
+++ proyectos/PROYECCION_INTELIGENTE/css/DarkMode.js
@@ -31,6 +31,62 @@ DarkMode.addEventListener('click', function(){
     console.log('nose');
 })
 
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="Servicios_cahnge">Aprobaciones Municipales</i>',
+        '<i class="Servicios_cahnge">Aprobaciones Estatales</i>',
+        '<i class="Servicios_cahnge">Requerimientos para la contrucción</i>',
+        '<i class="Servicios_cahnge">Régimen de condominio</i>',
+    ],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: true, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1600, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+    autoInsertCss: true,
+});
+
+const teamConteiner = document.querySelector('.team_conteiner');
+let maxScrollLeft = teamConteiner.scrollWidth - teamConteiner.clientWidth;
+
+
+let intervalo = null;
+let step = 1;
+
+const start = () => {
+    intervalo = setInterval(function() {
+    teamConteiner.scrollLeft = teamConteiner.scrollLeft + step;
+    if(teamConteiner.scrollLeft === maxScrollLeft){
+        step = (step * -1);
+    }else if(teamConteiner.scrollLeft ===  0){
+        step = (step * -1);
+    }
+    },10);
+}
+
+
+const stop = () => {
+    clearInterval(intervalo);
+}
+
+teamConteiner.addEventListener('mouseover', ()=>{
+    stop()
+})
+teamConteiner.addEventListener('mouseout', ()=>{
+    start()
+})
+
+start();
+
+
+
 
 
 
