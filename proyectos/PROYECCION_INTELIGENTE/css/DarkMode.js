10407bb2fc1f2c9e65e14a484cf58b819c253362
brancori <brandoncortesr7793@gmail.com>
2023-06-15 13:38:13 -0600

    cambios de servicios

--- proyectos/PROYECCION_INTELIGENTE/css/DarkMode.js
+++ proyectos/PROYECCION_INTELIGENTE/css/DarkMode.js
@@ -5,7 +5,6 @@ let logoPoryeccion = document.querySelector('#logo_proyeccion')
 
 let  var1 =  style.setProperty('--bg-color', '#f7f7f7');
 let  var2 = style.setProperty('--primary-color', '#e26122');
-let  var3 =  style.setProperty('--font-text', '#181818');
 let  var4 =  style.setProperty( '--button', '#f7f7f7');
 
 
@@ -23,7 +22,6 @@ DarkMode.addEventListener('click', function(){
     }else{
         var1 = style.setProperty('--bg-color', '#f7f7f7');
         var2 = style.setProperty('--primary-color', '#e26122');
-        var3 = style.setProperty('--font-text', '#181818');
         var4 =  style.setProperty( '--button', '#f7f7f7');
         mode.setAttribute("src", "./assets/img/dar_mode.png");
         logoPoryeccion.setAttribute('src', 'assets/img/proyecc_int.png')
