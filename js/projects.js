6c0cbcf8f79aac9bf3694ac2713fd2f069993e58
brand <brandoncortesr7793@gmail.com>
2025-03-18 16:44:09 -0600

    Add projects page with dynamic project listing and styles

---
+++ js/projects.js
@@ -0,0 +1,62 @@
+// Array con información de todos los proyectos
+const projects = [
+    {
+        id: 'sg',
+        title: 'GESTIÓN DE SEGURIDAD',
+        description: 'Sistema web para gestión de seguridad empresarial',
+        image: './proyectos/Sistema_Gestion/ASSETS/img/Gestion_logo.png',
+        link: './proyectos/Sistema_Gestion/index.html',
+        category: 'web'
+    },
+    {
+        id: 'trox',
+        title: 'PROVEDORES TROX',
+        description: 'Portal para proveedores de la empresa TROX',
+        image: './proyectos/TROX-Induction/assets/img/trox logo.gif',
+        link: './proyectos/TROX-Induction/Index.html',
+        category: 'web'
+    },
+    {
+        id: 'pi',
+        title: 'PROYECCIÓN INTELIGENTE',
+        description: 'Plataforma de análisis de datos predictivos',
+        image: './proyectos/PROYECCION_INTELIGENTE/assets/img/proyecc_int.png',
+        link: './proyectos/PROYECCION_INTELIGENTE/index.html',
+        category: 'design'
+    },
+    {
+        id: 'gc',
+        title: 'GOOGLE CLONE',
+        description: 'Recreación de la interfaz de Google',
+        image: './proyectos/google.html/img/google_preview.png',
+        link: './proyectos/google.html/index.html',
+        category: 'web'
+    },
+    {
+        id: 'lc',
+        title: 'LOBOCAFE',
+        description: 'Diseño web para cafetería artesanal',
+        image: './proyectos/LOBOCAFE/ASSETS/IMG/lobocafeina_.png',
+        link: './proyectos/LOBOCAFE/index.html',
+        category: 'design'
+    },
+    {
+        id: 'telcel',
+        title: 'EXAMEN TELCEL',
+        description: 'Plataforma de evaluación para Telcel',
+        image: './proyectos/telcel_quiz/assets/telcel_logo.jfif',
+        link: './proyectos/telcel_quiz/index.html',
+        category: 'web'
+    },
+    {
+        id: 'polar',
+        title: 'BIRRIA EL POLAR MENÚ',
+        description: 'Diseño de menú digital para restaurante',
+        image: './proyectos/BIRRIA EL POLAR/assets/img/orden_tacos.png',
+        link: './proyectos/BIRRIA EL POLAR/index.html',
+        category: 'design'
+    }
+];
+
+// Exportar el array para usarlo en diferentes archivos
+export default projects;
